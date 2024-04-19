fe5e3172a409f4348b5fcd8223bb9c6c7659a17c
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-04-19 13:15:56 +0800

    add toggle button for light mode

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+// Switch theme
+
+const body = document.querySelector("body");
+const toggle = document.querySelector(".header__switch");
+const footer = document.querySelector(".footer");
+
+console.log(body, toggle, footer);
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light");
+});
+
+toggle.addEventListener("click", () => {
+  footer.classList.toggle("light-bg");
+});
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+});
+
+// Swiper
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   slidesPerView: 3,
