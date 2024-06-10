f9bdbe4c449e8ba517ce3517abf053e862419023
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-06-10 10:41:22 +0500

    fix switch theme

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -8,20 +8,38 @@ export function setupThemeSwitch() {
     window.location.pathname === "/" ||
     window.location.pathname.endsWith("index.html");
 
-  const getPaths = (theme, isMainPage) => {
-    const basePath = isMainPage ? "./image/" : "../image/";
-    return {
-      mainLogo: `${basePath}header/nethammer-logo${
-        theme === "light" ? "-light" : ""
-      }.png`,
-      footerLogo: `${basePath}footer/nethammer-${
-        theme === "light" ? "blue" : "white"
-      }-logo.png`,
-      burgerLogo: `${basePath}header/burger${
-        theme === "light" ? "" : "-dark"
-      }.svg`,
+    const getPaths = (theme, isMainPage) => {
+      const basePath = isMainPage ? "./image/" : "../image/";
+      return {
+        mainLogo:
+          theme === "light"
+            ? `${basePath}header/nethammer-logo-light.png`
+            : `${basePath}header/nethammer-logo.png`,
+        footerLogo:
+          theme === "light"
+            ? `${basePath}footer/nethammer-blue-logo.png`
+            : `${basePath}footer/nethammer-white-logo.png`,
+        burgerLogo:
+          theme === "light"
+            ? `${basePath}header/burger.svg`
+            : `${basePath}header/burger-dark.svg`,
+      };
     };
-  };
+
+  // const getPaths = (theme, isMainPage) => {
+  //   const basePath = isMainPage ? "./image/" : "../image/";
+  //   return {
+  //     mainLogo: `${basePath}header/nethammer-logo${
+  //       theme === "light" ? "-light" : ""
+  //     }.png`,
+  //     footerLogo: `${basePath}footer/nethammer-${
+  //       theme === "light" ? "blue" : "white"
+  //     }-logo.png`,
+  //     burgerLogo: `${basePath}header/burger${
+  //       theme === "light" ? "" : "-dark"
+  //     }.svg`,
+  //   };
+  // };
 
   const changeImage = (imgElement, newSrc) => {
     imgElement.style.opacity = 0;
